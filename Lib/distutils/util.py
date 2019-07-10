622adb1a567b97573fad2c5b6d7575f5b1d4f95f
Suzumizaki-Kimitaka <szmml@h12u.com>
2019-07-11 06:31:59 +0900

    Enable to install extension library under the non-ASCII(Unicode) path.
    For example, consider to install cx_Freeze under the C:\my_project\私の仮想環境(\Lib\site-packages), created by venv on Windows. '私の作業環境' means 'my_virtual_environment' in Japanese, ofcourse non-ASCII.
    With open() function without 'encoding' parameter, writing the targets list of byte-compile, the encoding of the list is platform dependent. As of the list itself is also python code and all files are listed using full-path, byte compiling always fails.
    
    This patch will make the list always written in utf-8.

--- Lib/distutils/util.py
+++ Lib/distutils/util.py
@@ -386,12 +386,13 @@ def byte_compile (py_files,
         log.info("writing byte-compilation script '%s'", script_name)
         if not dry_run:
             if script_fd is not None:
-                script = os.fdopen(script_fd, "w")
+                script = os.fdopen(script_fd, "w", encoding="utf-8")
             else:
-                script = open(script_name, "w")
+                script = open(script_name, "w", encoding="utf-8")
 
             with script:
                 script.write("""\
+# -*- encoding: utf-8 -*-
 from distutils.util import byte_compile
 files = [
 """)
